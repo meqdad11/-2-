51ac91f0e66dc85a133d9b1f79f9e519200b87c0
meqdad11 <meqdadnagham375@gmail.com>
2026-05-26 13:37:30 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -5,8 +5,8 @@ from telegram import Update, ChatMemberUpdated
 from telegram.ext import ContextTypes
 from telegram.constants import ChatMemberStatus
 
-from . import database as db
-from .helpers import (
+from import database as db
+from helpers import (
     require_admin,
     is_admin,
     get_reply_user,
