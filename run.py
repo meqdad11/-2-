e4feb7a02f028de92ebbea12a34635c88c3b6c9c
meqdad11 <meqdadnagham375@gmail.com>
2026-06-01 14:46:48 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 # Add the bot folder to path so imports work without package structure
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "bot"))
 
-from main import main
+from app import main
 
 if __name__ == "__main__":
     main()
