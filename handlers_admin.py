bfdb1701b88ec4d83fb33e53f0a5300e5d6325c7
meqdad11 <meqdadnagham375@gmail.com>
2026-06-04 10:48:37 +0300

    Update handlers_admin.py

--- handlers_admin.py
+++ handlers_admin.py
@@ -341,7 +341,6 @@ async def cmd_unmute(update: Update, context: ContextTypes.DEFAULT_TYPE):
             user_id=reply_user.id,
             permissions=ChatPermissions(
                 can_send_messages=True,
-                can_send_media_messages=True,
                 can_send_polls=True,
                 can_send_other_messages=True,
                 can_add_web_page_previews=True
