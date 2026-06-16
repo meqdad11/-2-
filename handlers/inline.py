92759b61f6b4e42333690fb39abed3973ed73eb9
meqdad11 <meqdadnagham375@gmail.com>
2026-06-16 16:56:09 +0300

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -1,5 +1,8 @@
-async def handle_inline_query(update, context):
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def handle_inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
     pass
 
-async def handle_chosen_inline_result(update, context):
+async def handle_chosen_inline_result(update: Update, context: ContextTypes.DEFAULT_TYPE):
     pass
